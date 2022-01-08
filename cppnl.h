d2cfd0268104f93ee82c8ea40336a530b36c4243
Tripletth <you@example.com>
2022-01-08 13:47:24 +0100

    aanvankelijke vastlegging

---
+++ cppnl.h
@@ -0,0 +1,70 @@
+#ifndef CPPNL_H_INCLUDED
+#define CPPNL_H_INCLUDED
+
+//asm
+//auto
+//bool
+#define breek break
+#define geval case
+#define vang catch
+#define kar char
+#define klasse class
+//const
+#define const_giet const_cast
+#define verdergaan continue
+#define standaard default
+#define verwijder delete
+#define verkltype decltype
+#define doe do
+#define dubbel double
+#define dynamisch_giet dynamic_cast
+#define anders else
+#define opsom enum
+#define expliciet explicit
+#define exporteer export
+#define buiten extern
+//extern "C"
+#define onwaar false
+#define zweef float
+#define voor for
+#define vriend friend
+#define ganaar goto
+#define als if
+#define binnenlijn inline
+//int
+#define lang long
+#define muteerbaar mutable
+#define naamruimte namespace
+#define nieuw new
+//operator
+#define prive private
+#define beschermd protected
+#define openbaar public
+//register
+#define herinterpreteer_giet reinterpret_cast
+#define geefterug return
+#define kort short
+#define getekend signed
+#define groottevan sizeof
+#define statisch static
+#define statisch_giet static_cast
+#define structuur struct
+#define schakel switch
+#define sjabloon template
+#define dit this
+#define gooi throw
+#define waar true
+#define probeer try
+//typedef
+//typeid
+#define typenaam typename
+#define vereniging union
+#define ongetekend unsigned
+#define gebruikt using
+#define virtueel virtual
+#define leeg void
+#define vergangkelijk volatile
+#define wkar_t wchar_t
+#define terwijl while
+
+#endif // CPPNL_H_INCLUDED
